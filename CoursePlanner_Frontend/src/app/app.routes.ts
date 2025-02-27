83a28ca524f47cc147243e8e659b61dc749271cf
NuckJunes <najones47@outlook.com>
2025-02-27 09:27:54 -0500

    Added basic navbar with working routes

--- CoursePlanner_Frontend/src/app/app.routes.ts
+++ CoursePlanner_Frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MainScheduleComponent } from './main-schedule/main-schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'profile', component: ProfileComponent},
+    { path: 'schedule', component: MainScheduleComponent}
+];
+
+export const routing = RouterModule.forRoot(routes);
---
+++ CoursePlanner_Frontend/src/app/main-schedule/main-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+
+@Component({
+  selector: 'app-main-schedule',
+  imports: [NavbarComponent],
+  templateUrl: './main-schedule.component.html',
+  styleUrl: './main-schedule.component.css'
+})
+export class MainScheduleComponent {
+
+}
